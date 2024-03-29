11588749f15efabdd38c33b58d276e887137061b
itwill <itwill@DESKTOP-MV2KVS2>
2024-03-29 18:30:02 +0900

    111Day

--- spring/src/main/java/hewon_view.java
+++
@@ -1,4 +0,0 @@
-
-public class hewon_view {
-
-}
---
+++ spring/src/main/java/xyz/itwill09/service/StudentService.java
@@ -0,0 +1,11 @@
+package xyz.itwill09.service;
+
+import java.util.List;
+
+import xyz.itwill09.dto.Student;
+
+public interface StudentService {
+	void addStudent(Student student);
+	List<Student> getStudentList();
+
+}
